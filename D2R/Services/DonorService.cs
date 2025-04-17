6b0e1c1be8426b0c2e888b172d271a94a7ca2887
Hhacute <promax2807@gmail.com>
2025-04-17 21:48:52 +0700

    demo AddDonor and check validation input (ValidatinHelper.cs)

--- D2R/Services/DonorService.cs
+++ D2R/Services/DonorService.cs
@@ -1,7 +1,9 @@
-using D2R.Models;
+﻿using D2R.Models;
 using D2R.Repositories;
 using System.Collections.Generic;
-
+using System.Text.RegularExpressions;
+using D2R.Helpers;
+using System.Windows;
 namespace D2R.Services
 {
     public class DonorService
@@ -25,8 +27,29 @@ namespace D2R.Services
 
         public void Add(Donor entity)
         {
+            try
+            {
+    if (string.IsNullOrWhiteSpace(entity.FullName))
+                throw new ArgumentException("Tên không được để trống.");
+            if (!ValidationHelper.IsValidCanCuocCongDan(entity.Cccd))
+                throw new ArgumentException("Sai kiểu mẫu của căn cước công dân");
+            if (!ValidationHelper.IsValidPhoneNumber(entity.Phone))
+                throw new ArgumentException("Sai kiểu mẫu của số điện thoại");
+            if (!ValidationHelper.IsValidEmailAddress(entity.Email))
+            throw new ArgumentException("Sai định dạng Email");
             _repository.Add(entity);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Dữ liệu không hợp lệ", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
+        
+
 
         public void Update(Donor entity)
         {
